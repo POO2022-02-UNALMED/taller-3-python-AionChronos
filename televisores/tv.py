63e483387237740131244f3c14e502430f361e46
Julián Orrego Martínez <70676228+AionChronos@users.noreply.github.com>
2022-08-30 00:05:47 -0500

    anotadores off

--- televisores/tv.py
+++ televisores/tv.py
@@ -5,7 +5,6 @@ class TV:
     def __init__ (self, marca, estado):
         self.marca = marca
         self.estado = estado
-
         self.canal = 1
         self.precio = 500
         self.volumen = 1
@@ -44,45 +43,35 @@ class TV:
     def getCanal (self):
         return self.canal
 
-#turnOn
 
     def turnOn (self):
         if self.estado == False:
             self.estado = True
-     
-#turnOff
 
     def turnOff (self):
         if self.estado == True:
             self.estado = False
 
-#getEstado
     def getEstado (self):
         return self.estado
     
-# volumenUp
     def volumenUp (self):
         if (self.getEstado() == True and self.getVolumen() < 7):
             self.volumen += 1
 
-# volumeDown
     def volumenDown (self):
         if (self.getEstado() == True and self.getVolumen() > 0):
             self.volumen -= 1
 
-# canalnUp
     def canalUp (self):
         if (self.getEstado() == True):
             if (self.getCanal() < 120):
                 self.canal += 1
             
-# canalDown
     def canalDown (self): 
         if (self.getEstado() ): 
             if (self.getCanal() > 1):
                 self.canal -= 1
-
-#Set and get NumTV
     def setNumTV (num):
         TV.numTV =  num
     
